404fc6c93ce99552e172d8fa63e1dad5837a68a9
Andika Alakate <andikaalakate@gmail.com>
2024-04-30 12:48:39 +0700

    Install Filament, Mix, TailwindCSS, dan AlpineJS

---
+++ webpack.mix.cjs
@@ -0,0 +1,13 @@
+const mix = require('laravel-mix');
+const tailwindcss = require("tailwindcss");
+
+mix.browserSync('127.0.0.1:8000');
+
+mix
+.js("resources/js/alpine.js", "js")
+.postCss(
+    "resources/css/app.css", // File masukan (input file)
+    "css", // Folder keluaran (output folder)
+    [tailwindcss("tailwind.config.js")] // Plugin Tailwind CSS
+)
+.setPublicPath('public');
